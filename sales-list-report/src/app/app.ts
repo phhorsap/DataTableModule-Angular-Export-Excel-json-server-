10228551bda4ca4333dac1063ba0e42aacc225dc
phhorsap <43028614+phhorsap@users.noreply.github.com>
2025-08-25 19:53:25 +0000

    datatable setting

--- sales-list-report/src/app/app.ts
+++ sales-list-report/src/app/app.ts
@@ -1,9 +1,10 @@
 import { Component, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { TableList } from './table-list/table-list';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, TableList],
   templateUrl: './app.html',
   styleUrl: './app.scss'
 })
--- sales-list-report/src/app/table-list/table-list.ts
+++ sales-list-report/src/app/table-list/table-list.ts
@@ -1,11 +1,26 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { DataTablesModule } from 'angular-datatables';
+import { Config } from 'datatables.net';
+
 
 @Component({
   selector: 'app-table-list',
-  imports: [],
+  imports: [DataTablesModule],
   templateUrl: './table-list.html',
   styleUrl: './table-list.scss'
 })
-export class TableList {
+export class TableList implements OnInit {
 
+  tableData: any[] = [];
+  
+
+  ngOnInit(): void {
+   
+    // Beispiel-Daten
+    this.tableData = [
+      { id: 1, name: 'Produkt A', preis: 10 },
+      { id: 2, name: 'Produkt B', preis: 20 },
+      { id: 3, name: 'Produkt C', preis: 30 }
+    ];
+  }
 }
